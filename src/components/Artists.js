a19e759ae8b306ea329f8f1c073b55d9431f9438
Michelle Lim <elle@mmmanyfold.com>
2021-07-20 09:52:28 -0600

    add no results text

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Artists = (props) => {
+const Artists = ({ data, visible }) => {
   return (
-    <div className="grid" style={!props.visible ? { display: 'none' } : null}>
-      {props.data.map((item) => (
+    <div className="grid" style={!visible ? { display: 'none' } : null}>
+      {data.map((item) => (
         <div key={item.id} className="grid-item">
           <div className="artist-card">{item.name}</div>
         </div>
       ))}
+      {!data.length && <div className="no-results">No artists match your criteria.</div>}
     </div>
   );
 };
