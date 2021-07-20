d21bbc79501da350a38aa1ef4004bd48c23da102
Michelle Lim <elle@mmmanyfold.com>
2021-07-20 00:49:27 -0600

    add prettier & format on commit

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 
 const Artists = (props) => {
-    return (
-        <div className="grid" style={!props.visible ? { display: 'none' } : null}>
-            {props.data.map(item => (
-                <div key={item.id} className="grid-item">
-                    <div className="artist-card">
-                        {item.name}
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="grid" style={!props.visible ? { display: 'none' } : null}>
+      {props.data.map((item) => (
+        <div key={item.id} className="grid-item">
+          <div className="artist-card">{item.name}</div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Artists
+export default Artists;
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,53 +1,53 @@
 import React, { useState } from 'react';
 
 const Filters = (props) => {
-    const [activeTags, setActiveTags] = useState([])
+  const [activeTags, setActiveTags] = useState([]);
 
-    const isSelected = (tag) => {
-        return activeTags.some(t => t.id === tag.id)
+  const isSelected = (tag) => {
+    return activeTags.some((t) => t.id === tag.id);
+  };
+
+  const tagStyle = (tag) => {
+    let style = 'tag';
+    if (isSelected(tag)) {
+      style += ' tag-selected';
     }
+    return style;
+  };
 
-    const tagStyle = (tag) => {
-        let style = "tag"
-        if (isSelected(tag)) {
-            style += " tag-selected"
-        }
-        return style
+  const handleSelect = (tag) => {
+    let active;
+    if (isSelected(tag)) {
+      active = activeTags.filter((t) => t.id !== tag.id);
+    } else {
+      active = [...activeTags, tag];
     }
+    setActiveTags(active);
+    props.onFilter(active);
+  };
 
-    const handleSelect = (tag) => {
-        let active;
-        if (isSelected(tag)) {
-            active = activeTags.filter(t => t.id !== tag.id)
-        } else {
-            active = [...activeTags, tag]
-        }
-        setActiveTags(active)
-        props.onFilter(active)
-    }
-
-    return (
-        <div className="tag-list" style={!props.visible ? { display: 'none' } : null}>
-            {props.data.map((tag) => (
-                <div key={tag.id}>
-                    <input
-                        type="checkbox"
-                        id={tag.id}
-                        name="tag"
-                        value={isSelected(tag)}
-                        style={{ display: "none" }}
-                    />
-                    <div
-                        htmlFor={tag.id} 
-                        onClick={() => handleSelect(tag)}
-                        className={tagStyle(tag)}
-                    >
-                        {tag.name}
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="tag-list" style={!props.visible ? { display: 'none' } : null}>
+      {props.data.map((tag) => (
+        <div key={tag.id}>
+          <input
+            type="checkbox"
+            id={tag.id}
+            name="tag"
+            value={isSelected(tag)}
+            style={{ display: 'none' }}
+          />
+          <div
+            htmlFor={tag.id}
+            onClick={() => handleSelect(tag)}
+            className={tagStyle(tag)}
+          >
+            {tag.name}
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Filters
+export default Filters;
