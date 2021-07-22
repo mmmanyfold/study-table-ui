c5b82a3db95863456a4a6fc30546a96e653028ae
Michelle Lim <elle@mmmanyfold.com>
2021-07-22 00:58:34 -0600

    make search bar & filters fixed

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -14,30 +14,32 @@ const Filters = ({ data, active, visible, onSelect, onClear }) => {
   };
 
   return (
-    <div className="tags-container" style={!visible ? { display: 'none' } : null}>
-      <div className="tags-header">
-        {active.length ? (
-          <div role="button" onClick={onClear} style={{ cursor: 'pointer' }}>
-            Clear All {!!active.length && ` (${active.length})`}
-          </div>
-        ) : (
-          <div>Filter by Tags</div>
-        )}
-      </div>
-      {data.map((tag) => (
-        <div key={tag.id} className="tag-wrapper">
-          <input
-            type="checkbox"
-            id={tag.id}
-            name="tag"
-            value={isActive(tag)}
-            style={{ display: 'none' }}
-          />
-          <div htmlFor={tag.id} onClick={() => onSelect(tag)} className={tagStyle(tag)}>
-            {tag.name}
-          </div>
+    <div className="filters-scrollview" style={!visible ? { display: 'none' } : null}>
+      <div className="filters-container">
+        <div className="filters-header">
+          {active.length ? (
+            <div role="button" onClick={onClear} style={{ cursor: 'pointer' }}>
+              Clear All {!!active.length && ` (${active.length})`}
+            </div>
+          ) : (
+            <div>Filter by Tags</div>
+          )}
         </div>
-      ))}
+        {data.map((tag) => (
+          <div key={tag.id} className="tag-wrapper">
+            <input
+              type="checkbox"
+              id={tag.id}
+              name="tag"
+              value={isActive(tag)}
+              style={{ display: 'none' }}
+            />
+            <div htmlFor={tag.id} onClick={() => onSelect(tag)} className={tagStyle(tag)}>
+              {tag.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
