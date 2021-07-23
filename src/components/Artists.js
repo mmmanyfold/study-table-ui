c3eaf582740344c1b5834b3c31283b8e3def8ba9
Michelle Lim <elle@mmmanyfold.com>
2021-07-23 01:27:22 -0600

    open artist view on click

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Artists = ({ data, visible }) => {
+function Artists({ data, visible, windowSize, mobile }) {
+  const [selected, setSelected] = useState(null);
+
+  const handleClick = (item) => {
+    setSelected(selected?.id === item.id ? null : item);
+  };
+
+  const onReturn = () => {
+    setSelected(null);
+  };
+
   return (
     <div className="grid-scrollview" style={!visible ? { display: 'none' } : null}>
       <div className="grid">
         {data.map((item) => (
           <div key={item.id} className="grid-item">
-            <div className="artist-card">
+            <div className="artist-card" onClick={() => handleClick(item)}>
               <div
                 className="artist-image"
                 style={{
@@ -31,9 +41,34 @@ const Artists = ({ data, visible }) => {
         {!data.length && (
           <div className="no-results">No artists match your criteria.</div>
         )}
+        <ArtistView
+          artist={selected}
+          onReturn={onReturn}
+          windowSize={windowSize}
+          mobile={mobile}
+        />
       </div>
     </div>
   );
-};
+}
 
 export default Artists;
+
+function ArtistView({ artist, onReturn, windowSize, mobile }) {
+  if (!artist) {
+    return null;
+  }
+  return (
+    <div
+      className="grid-item selected"
+      style={{ width: mobile ? '100%' : windowSize.width - 248 }}
+    >
+      <div className="artist-card selected" onClick={onReturn}>
+        <div>{artist.name}</div>
+        <div>{artist.img}</div>
+        <div>{artist.info}</div>
+        <div>{artist.tags}</div>
+      </div>
+    </div>
+  );
+}
