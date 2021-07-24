bd4bb00a80ab29c4b3d3f9d71c8c48f5edcb1d6a
Michelle Lim <elle@mmmanyfold.com>
2021-07-24 16:42:52 -0600

    update ArtistView component, not sure if will be used

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-function ArtistView({ artist }) {
+function ArtistView({ artist, onReturn, windowSize, mobile }) {
   if (!artist) {
     return null;
   }
   return (
-    <div>
-      <div>{artist.name}</div>
-      <div>{artist.img}</div>
-      <div>{artist.info}</div>
-      <div>{artist.tags}</div>
+    <div
+      className="grid-item selected"
+      style={{ width: mobile ? '100%' : windowSize.width - 248 }}
+    >
+      <div className="artist-card selected" onClick={onReturn}>
+        <div>{artist.name}</div>
+        <div>{artist.img}</div>
+        <div>{artist.info}</div>
+        <div>{artist.tags}</div>
+      </div>
     </div>
   );
 }
--- src/components/Artists.js
+++ src/components/Artists.js
@@ -59,34 +59,9 @@ function Artists({ data, visible, windowSize, mobile }) {
         {!data.length && (
           <div className="no-results">No artists match your criteria.</div>
         )}
-        {/* <ArtistView
-          artist={selectedArtist}
-          onReturn={onReturn}
-          windowSize={windowSize}
-          mobile={mobile}
-        /> */}
       </div>
     </div>
   );
 }
 
 export default Artists;
-
-// function ArtistView({ artist, onReturn, windowSize, mobile }) {
-//   if (!artist) {
-//     return null;
-//   }
-//   return (
-//     <div
-//       className="grid-item selected"
-//       style={{ width: mobile ? '100%' : windowSize.width - 248 }}
-//     >
-//       <div className="artist-card selected" onClick={onReturn}>
-//         <div>{artist.name}</div>
-//         <div>{artist.img}</div>
-//         <div>{artist.info}</div>
-//         <div>{artist.tags}</div>
-//       </div>
-//     </div>
-//   );
-// }
