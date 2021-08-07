efe43a9b2fbeaaeac5817a5997d07bab97749741
Michelle Lim <oreneemshi@gmail.com>
2021-08-07 17:02:54 -0600

    Set tags heading fixed while scrolling (#17)
    * set tags heading fixed while scrolling
    
    * line up filters heading and tags padding

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -15,11 +15,8 @@ const Filters = ({ data, active, visible, windowSize, onSelect, onClear }) => {
   };
 
   return (
-    <div
-      className="filters-scrollview"
-      style={{ height: windowSize.height, display: visible ? 'block' : 'none' }}
-    >
-      <div className="filters-container">
+    <div>
+      {visible && (
         <div className="filters-header">
           {active.length ? (
             <div role="button" onClick={onClear} style={{ cursor: 'pointer' }}>
@@ -27,23 +24,34 @@ const Filters = ({ data, active, visible, windowSize, onSelect, onClear }) => {
               <img src={CloseIcon} alt="X Icon" style={{ marginLeft: '8px' }} />
             </div>
           ) : (
-            <div>Filter by tags</div>
+            <div>Tags</div>
           )}
         </div>
-        {data.map((tag) => (
-          <div key={tag.id} className="tag-wrapper">
-            <input
-              type="checkbox"
-              id={tag.id}
-              name="tag"
-              value={isActive(tag)}
-              style={{ display: 'none' }}
-            />
-            <div htmlFor={tag.id} onClick={() => onSelect(tag)} className={tagStyle(tag)}>
-              <div>{tag.name}</div>
+      )}
+      <div
+        className="filters-scrollview"
+        style={{ height: windowSize.height, display: visible ? 'block' : 'none' }}
+      >
+        <div className="filters-container">
+          {data.map((tag) => (
+            <div key={tag.id} className="tag-wrapper">
+              <input
+                type="checkbox"
+                id={tag.id}
+                name="tag"
+                value={isActive(tag)}
+                style={{ display: 'none' }}
+              />
+              <div
+                htmlFor={tag.id}
+                onClick={() => onSelect(tag)}
+                className={tagStyle(tag)}
+              >
+                <div>{tag.name}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
